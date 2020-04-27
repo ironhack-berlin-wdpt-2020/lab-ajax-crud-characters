7b0804200234c901c8a23f2f8c1703021d379bb1
Presenter <presenter@Hendriks-MBP-2.fritz.box>
2020-04-27 11:09:37 +0200

    adds fetch all

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,24 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+    // clone the character-info section and then remove it from the DOM
+    let templateNode = document.getElementsByClassName('character-info')[0].cloneNode(true)
+    document.getElementsByClassName('character-info')[0].remove()
+
+    axios.get('http://localhost:8000/characters').then((response) => {
+      response.data.forEach((character) => {
+
+        let newNode = templateNode.cloneNode(true)
+        newNode.getElementsByClassName('name')[0].innerText = character.name
+        newNode.getElementsByClassName('occupation')[0].innerText = character.occupation
+        newNode.getElementsByClassName('cartoon')[0].innerText = character.cartoon
+        newNode.getElementsByClassName('weapon')[0].innerText = character.weapon
+        document.getElementsByClassName('characters-container')[0].append(newNode)
+
+      })
+    })
+
 
   });
 
